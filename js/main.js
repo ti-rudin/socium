774a5fd7446cef8636ce4ca721e6a5709f3dd548
ti-rudin <rudin@ti-units.ru>
2017-01-19 02:44:05 +0300

    3

---
+++ js/main.js
@@ -0,0 +1,21 @@
+window.onload=function(){i=7;
+for (index = 1; index <= i; index++) {
+  //Создадим объект obj
+  var obj = new Object();
+  //Добавим объекту obj свойство name со значением 'Дмитрий'
+  obj.name = 'Дмитрий' + index;
+  //Добавим объекту obj свойство age со значением 26
+  obj['age'] = 26 + index;
+  //Обратимся к свойствам объекта для вывода их значений
+  var canvas = document.getElementById('circle');
+    var obCanvas = canvas.getContext('2d');
+    obCanvas.beginPath();
+    obCanvas.arc(100+(index*20), 75, 10, 0, 2*Math.PI, false);
+    obCanvas.fillStyle = 'red';
+    obCanvas.fill();
+    obCanvas.lineWidth = 1;
+    obCanvas.strokeStyle = 'red';
+    obCanvas.stroke();
+
+}
+}
