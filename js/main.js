11336443b64de0fed45b319de689532965df0bd6
ti-rudin <rudin@ti-units.ru>
2017-01-21 21:25:31 +0300

    кирилл

--- js/main.js
+++ js/main.js
@@ -1,76 +1,84 @@
 
-window.requestAnimFrame = (function(callback) {
-        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
-        function(callback) {
-          window.setTimeout(callback, 1000 / 60);
-        };
-      })();
-window.onload=function(){
-
-var arr = new Array();
-for(i=0;i<5;i++){
-arr[i]= new Array();
-for(j=0;j<10;j++){
-    s=(i+1)*10+j-10;
-    arr[i][j]= s;
-}
-}
-
-
-var WidthSpace=500;
-var HeightSpace=500;
-var step=50;
-var stepx=WidthSpace/step;
-var stepy=HeightSpace/step;
-
-var space = new Array();
-var sum=1;
-for (i = 0; i <= (stepx-1); i++) {
-    space[i]=new Array();
-    for (j = 0; j <= (stepy-1); j++) {
-        space[i][j]=sum;//заполнили массив
-        sum++;
-        }
-    }
-console.log(space);
-    
-var canvas = document.getElementById('myCanvas');
-var context = canvas.getContext('2d');
-function rand (min, max)
-{
+window.onload = function () {
+function rand(min, max) {
   min = parseInt(min);
   max = parseInt(max);
   return Math.floor( Math.random() * (max - min + 1) ) + min;
 }
-var obj = new Object();    
-for (i = 0; i <= stepx-1; i++) {
-    for (j = 0; j <= stepy-1; j++) {
+    var space = [];
+    var lud = [];
+    var pop = 40;
+    var WidthSpace = 500;
+    var HeightSpace = 500;
+    var step = 30;
+    var stepx = WidthSpace / step;
+    var stepy = HeightSpace / step;
 
+
+
+    for (i = 0; i <= (stepx - 1); i++) {
+        space[i] = [];
+        for (j = 0; j <= (stepy - 1); j++) {
+        space[i][j] = 0;//обнулили массив
+        }
+    }
+
+console.log(space);
+    
+space[2][2] = 4;
+space[4][5] = 7;
+space[1][3] = 13;
+space[7][2] = 5; //вручную разместили первых 4
+
+
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
+
+var s=1;
+function render (){
+for (i = 0; i <= stepx - 1; i++) {
+    for (j = 0; j <= stepy - 1; j++) {
+
+if (space[i][j]>0) {
         
-      obj.name = space[i][j];   
-      obj.x=i; 
-      obj.y=j;
-
 
       context.font = '10pt Calibri';
       context.fillStyle = 'blue';
       //context.textAlign = 'center';
-      context.fillText(obj.name, (j+1)*step-5, (i+1)*step+5);
+      context.fillText(space[i][j], (i+1)*step-5, (j+1)*step+5);
 
-      var radius = 20;
+      var radius = 25;
       context.beginPath();
-      context.arc((obj.x+1)*step, (obj.y+1)*step, radius, 0, 2 * Math.PI, false);
+      context.arc((i+1)*step, (j+1)*step, radius, 0, 2 * Math.PI, false);
       context.lineWidth = 1;
 
       // line color
       context.strokeStyle = 'red';
       context.stroke();
+      }
 //console.log(obj);
+      }
+    }
+}
+function calculate (){
+  for (i = 0; i <= stepx - 1; i++) {
+    for (j = 0; j <= stepy - 1; j++) {
+      if (space[i][j]>0) {
+          a=i+1;
+          space[a][j]=space[i][j];
+          space[i][j]=0;
 
-
-        }
+      }
 
     }
+  }
+}
+    render();
+calculate ();
+        render();
+calculate ();    render();
+calculate ();    render();
+calculate ();
 
 
 
