763542dff808502f4b8630671aa5768bd5691507
ti-rudin <rudin@ti-units.ru>
2017-01-21 14:08:36 +0300

    сделал матрицу

--- js/main.js
+++ js/main.js
@@ -1,23 +1,62 @@
 window.onload=function(){
-i=7;
-var sex = ["М","Ж"];
-var man = new Array();
-    man[0]
-for (index = 1; index <= i; index++) {
-  var obj = new Object();
-  obj.name = 'й' + index;
-  obj['age'] = 26 + index;
 
-  var canvas = document.getElementById('circle');
-    var obCanvas = canvas.getContext('2d');
-    obCanvas.beginPath();
-    obCanvas.arc(101+(index*20), 75-index*5, 5+index*0.5, 0, 2*Math.PI, false);
-    obCanvas.fillStyle = 'rgba(214, 31, 31, 1)';
-    obCanvas.fill();
-    obCanvas.lineWidth = 1;
-    obCanvas.strokeStyle = 'red';
-    obCanvas.stroke();
-    obCanvas.fillText(i, 150+index*10, 100);
 
+var arr = new Array();
+for(i=0;i<5;i++){
+arr[i]= new Array();
+for(j=0;j<10;j++){
+    s=(i+1)*10+j-10;
+    arr[i][j]= s;
 }
+}
+
+
+var WidthSpace=500;
+var HeightSpace=500;
+var step=100;
+stepx=WidthSpace/step;
+stepy=HeightSpace/step;
+
+var space = new Array();
+for (i = 0; i <= (stepx-1); i++) {
+    space[i]=new Array();
+    for (j = 0; j <= (stepy-1); j++) {
+
+        sum=(i+1)*10+j-9;
+        space[i][j]=sum;//заполнили массив
+
+        }
+
+    }
+console.log(space);
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
+
+for (i = 0; i <= stepx-1; i++) {
+    for (j = 0; j <= stepy-1; j++) {
+
+        var obj = new Object();
+        obj.name = space[i][j];
+
+
+      context.font = '10pt Calibri';
+      context.fillStyle = 'blue';
+      context.fillText(obj.name, j*step, i*step);
+
+      var radius = 55;
+      context.beginPath();
+      context.arc(i*step, j*step, radius, 0, 2 * Math.PI, false);
+      context.lineWidth = 1;
+
+      // line color
+      context.strokeStyle = 'red';
+      context.stroke();
+
+
+        }
+
+    }
+
+
+
 }
