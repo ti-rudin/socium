728e5f575542f3e0f697d0e962fe1183634028b9
ti-rudin <rudin@ti-units.ru>
2017-01-28 01:48:16 +0300

    проекции пространства

--- js/main.js
+++ js/main.js
@@ -19,6 +19,15 @@ window.onload = function () {
 //        context = canvas.getContext('2d');
 
 //ядровые функции    
+    function watch(xop,yop){
+        var idop = [];
+         for (var key in this.lud)   {
+        man = this.lud[key];
+        if ((man[0]=xop)&(man[1]=yop)) {idop.push(key)}
+
+         }
+        return idop
+    }
     
     function drawMan(man) {
         var x = man[0],
@@ -34,8 +43,8 @@ if (id > 0) {
         
         d.style.left = x;
         d.style.top = y;
-        d.style.width='10px';
-        d.style.height='10px';
+        d.style.width='3px';
+        d.style.height='3px';
         d.style.background=color;
         d.style.position='absolute';
         document.body.appendChild(d);
@@ -57,27 +66,44 @@ if (id > 0) {
 ///////  
     
 world = {
-    H: 500, 
-    W: 500,
+    H: 10,
+    W: 10,
     Day: 3,
     v: 1,
+    population : 5,
     lud: {x : 0,
           y : 0,
           r : 0,
           d : 0,
           c : 0,
-         id : 0}, 
+         id : 0},
+    planeX : {},
+    planeY : {},
     calculate: function(step) {
-   //смотрим направление и пересчитываем координаты
+//смотрим направление и пересчитываем координаты
     for (var key in this.lud)   {
         man = this.lud[key];
+
+//смотрим кто рядом, получаем массив ид
+
+
         id = man [5];
         if (id > 0) {
         switch (man[3]) {
                 case 1:{
                     man[0] = this.lud[key][0];
                     man[1] = this.lud[key][1]-step;
-                    if ((man[1]) < 0) {man[3] = 5; } //пересчитываем направление 
+                    if ((man[1]) < 0) {man[3] = 5; } //пересчитываем направление, если стенка
+                    //пересчитываем направление, если столкновение
+
+                    //смотрим координаты оппонента
+                    xop = man[0];
+                    yop = man[1];
+                    //смотрим id оппонента
+                    var idop = watch(xop,yop);
+                    console.log(xop,yop);
+                    if (idop!=0) {console.log(idop)};
+
                     }
                 break
                 case 2:{
@@ -149,21 +175,60 @@ world = {
        drawMan(man)}
     },
     createworld : function(){
+    //создаем пустое пространство
+        for (var i = 0; i < this.W; i++) {
+            this.planeX[i] = []
+        }
+         for (var i = 0; i < this.H; i++) {
+            this.planeY[i] = [];
+        }
 
-        for (var i = 0; i < 1000; i++) {
+    //создаем популяцию на основе случайных величин
+        for (var i = 0; i < this.population; i++) {
             world.lud[i]=[
                 (rand(1,world.W)),
                 (rand(1,world.H)),
-                (rand(1,2)),(rand(1,8)),
-                'red',i+1,];
+                (rand(1,2)),
+                (rand(1,8)),
+                'red',
+                i+1];
             l = document.createElement('div');
             l.id = i+1;
             document.body.appendChild(l);
-
+            px = world.lud[i][0];
+            py = world.lud[i][1];
+            world.planeX[px].push(i);
+            world.planeY[py].push(i);
+            //console.log(i,px,py,world.lud[i]);
+            //console.log();
             
     
         }
+       // console.log(world.lud);
+       // console.log(world.planeX,world.planeY);
         
+    },
+    transpon : function(){
+        //////////////
+    arr = this.lud;
+var ArrX = [];
+var ArrY = [];
+
+for(var i = 0; i < arr.length; i++){
+
+
+  for(var j = 0; j < arr[i].length; j++){
+    if(newArr.length <= j){
+      newArr.push([]);
+    }
+    newArr[j][i] = arr[i][j];
+  }
+}
+arr.splice(0, arr.length);
+for(var i = 0; i < newArr.length; i++){
+  arr.push(newArr[i])
+}
+        ///////////
         
         
     }
@@ -177,6 +242,7 @@ world = {
            
         world.render();
 <<<<<<< develop
+<<<<<<< develop
         world.calculate(2);
 //world.clear();    
         // request new frame
@@ -184,6 +250,9 @@ world = {
     
 =======
         world.calculate(3);
+=======
+        world.calculate(1);
+>>>>>>> проекции пространства
   
 >>>>>>> 12
         requestAnimFrame(function () {
@@ -198,13 +267,13 @@ world = {
     
 
 world.createworld();
-    console.log(world);
+ //   console.log(world);
         world.render();
-     world.calculate(2);
-    console.log(world);
+     world.calculate(1);
+   // console.log(world);
  
     
 startTime = (new Date()).getTime();
-//   setTimeout((animate(world,startTime)),1000);
+   setTimeout((animate(world,startTime)),1000);
 
 };
